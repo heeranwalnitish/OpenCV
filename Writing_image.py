205c770e9bd5b36aeb632793c1226132cf864133
Nitish kumar <43029769+heeranwalnitish@users.noreply.github.com>
2022-04-13 14:20:04 +0530

    Tutorial -2
    How to write an image

---
+++ Writing_image.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread('nick.jpg')
+
+cv2.imshow('Original Image', img)
+
+cv2.imwrite('Output.jpg', img)
+
+cv2.imwrite('Output.png', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
