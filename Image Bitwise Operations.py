64fca014a9141faab3933e4655a83fbd8af44a8e
Nitish kumar <43029769+heeranwalnitish@users.noreply.github.com>
2022-04-19 17:35:07 +0530

    Tutorial - 15
    Bit wise operation using images and videos.

---
+++ Image Bitwise Operations.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np 
+
+square = np.zeros((300,300), np.uint8)
+
+cv2.rectangle(square, (50,50), (150,150), 255, -2)
+cv2.imshow("Square",square)
+cv2.waitKey(0)
+
+
+# making a eclipse
+ellipse = np.zeros((300,300), np.uint8)
+cv2.ellipse(ellipse, (150,150), (150, 150), 30, 0, 180, 255, -1)
+cv2.imshow("Ellipse", ellipse)
+cv2.waitKey(0)
+
+And = cv2.bitwise_and(square, ellipse)
+cv2.imshow("AND", And)
+cv2.waitKey(0)
+
+Or = cv2.bitwise_or(square, ellipse)
+cv2.imshow("OR", Or)
+cv2.waitKey(0)
+
+Xor = cv2.bitwise_xor(square, ellipse)
+cv2.imshow("Xor", Xor)
+cv2.waitKey(0)
+
+
+Not = cv2.bitwise_not(square)
+cv2.imshow("NOT", Not)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+ 
