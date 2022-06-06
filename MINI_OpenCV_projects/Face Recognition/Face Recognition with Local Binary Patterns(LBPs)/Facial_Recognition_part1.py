8c965a9af5778ea65eb0e565801c4146f9974622
Nitish kumar <43029769+heeranwalnitish@users.noreply.github.com>
2022-06-06 18:38:54 +0530

    Add files via upload

---
+++ MINI_OpenCV_projects/Face Recognition/Face Recognition with Local Binary Patterns(LBPs)/Facial_Recognition_part1.py
@@ -0,0 +1,53 @@
+# Collecting the face samples
+
+import cv2
+from isort import file 
+import numpy as np
+
+
+faceDetecter = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+def face_extractor(img):
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceDetecter.detectMultiScale(gray_img,1.3,5)
+
+    if faces is ():
+        return None
+
+    for (x,y,w,h) in faces:
+        cropped_faces = img[y:y+h, x:x+w]
+
+    return cropped_faces
+
+
+cap = cv2.VideoCapture(0)
+count = 0
+
+while True:
+    ret, frame = cap.read()
+    if face_extractor(frame) is not None :
+        count+=1
+
+        face = cv2.resize(face_extractor(frame),(200,200))
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+        file_name_path = r'C:\Users\Admin\Computer vision\Opencv_project\Facial Recognition System\Face_sample\user'+str(count)+'.jpg'
+
+        cv2.imwrite(file_name_path, face)
+
+        cv2.putText(face, str(count), (50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0), 2)
+        cv2.imshow('Face Cropper', face) 
+
+    else:
+        print("Face Not Detected")
+        pass
+
+    if cv2.waitKey(1) ==13 or count == 100:
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
+
+print("Collecting Sample Successfull")
